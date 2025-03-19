2e9931c60b2d170e9144db88689fc23315ae54ff
3mr00 <amrjok00@gmail.com>
2025-03-19 16:37:10 +0200

    v2 i18n multi fils

--- i18n.ts
+++ i18n.ts
@@ -1,33 +1,13 @@
 import { getRequestConfig } from "next-intl/server";
-import fs from "fs";
-import path from "path";
-
-// Helper function to merge JSON files
-const mergeMessages = (locale: string) => {
-  try {
-    const messagesDir = path.join(process.cwd(), "messages", locale);
-    if (!fs.existsSync(messagesDir)) {
-      console.error(`Locale directory not found: ${messagesDir}`);
-      return {};
-    }
-
-    const files = fs.readdirSync(messagesDir);
-    return files.reduce((acc, file) => {
-      if (file.endsWith(".json")) {
-        const filePath = path.join(messagesDir, file);
-        const fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-        return { ...acc, ...fileContent };
-      }
-      return acc;
-    }, {});
-  } catch (error) {
-    console.error(`Error loading messages for locale ${locale}:`, error);
-    return {};
-  }
-};
 
 export default getRequestConfig(async ({ locale }) => {
   return {
-    messages: mergeMessages(locale),
+    messages: {
+      ...(await import(`./messages/${locale}/home.json`)).default,
+      ...(await import(`./messages/${locale}/lang.json`)).default,
+      ...(await import(`./messages/${locale}/productPage.json`)).default,
+      ...(await import(`./messages/${locale}/technologyPage.json`)).default,
+      ...(await import(`./messages/${locale}/turboPage.json`)).default,
+    },
   };
 });
